6eb37d4a65edaa6b9ef4e9ea5ddf3d8a506246f2
Kickblip <wyatt.zilker@gmail.com>
2025-12-07 20:19:24 -0600

    adding clouds to map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Image from "next/image"
+import { motion } from "motion/react"
 
 const IMAGE_WIDTH = 2048
 const IMAGE_HEIGHT = 1152
@@ -14,13 +17,29 @@ const points = [
 
 export default function Map() {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden">
       <Image
-        src="/map_terrain3.jpg"
+        src="/map_terrain.jpg"
         alt="Map"
         width={IMAGE_WIDTH}
         height={IMAGE_HEIGHT}
-        className="opacity-80 pointer-events-none aspect-video select-none"
+        className="opacity-80 pointer-events-none select-none"
+      />
+
+      <motion.div
+        className="
+          pointer-events-none select-none absolute inset-0
+          bg-[url('/clouds.jpg')]
+          bg-repeat
+          opacity-25
+        "
+        initial={{ backgroundPositionX: "0px" }}
+        animate={{ backgroundPositionX: "-512px" }}
+        transition={{
+          duration: 50,
+          repeat: Infinity,
+          ease: "linear",
+        }}
       />
 
       <div className="pointer-events-none absolute inset-0 shadow-[inset_0_0_45px_40px_rgb(9,9,11)]" />
@@ -28,7 +47,7 @@ export default function Map() {
       {points.map((p) => (
         <div
           key={p.id}
-          className="absolute h-3 w-3 rounded-full bg-blue-500 shadow
+          className="absolute z-20 h-3 w-3 rounded-full bg-blue-500 shadow
                      -translate-x-1/2 -translate-y-1/2"
           style={{
             left: `${(p.x / IMAGE_WIDTH) * 100}%`,
