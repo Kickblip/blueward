953bf7211b12dca39cbaa481bf53948d8230c9f4
Kickblip <wyatt.zilker@gmail.com>
2026-01-07 16:13:18 -0600

    Schema updates

--- apps/web/lib/schema.ts
+++ apps/web/lib/schema.ts
@@ -1,5 +1,5 @@
-import { integer, pgTable, varchar, timestamp, bigint, doublePrecision, boolean, pgEnum, uniqueIndex } from "drizzle-orm/pg-core"
-import { relations } from "drizzle-orm"
+import { integer, pgTable, varchar, timestamp, bigint, boolean, pgEnum, real, smallint } from "drizzle-orm/pg-core"
+import { relations, sql } from "drizzle-orm"
 
 // matches table -> 1 row per game
 // objectives table -> 1 row per team per game (2 rows per game)
@@ -10,6 +10,7 @@ export const matches = pgTable("matches", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
   matchId: varchar({ length: 32 }).notNull().unique(),
+  tag: varchar({ length: 8 }).notNull().default("INHOUSE"),
   players: varchar({ length: 128 }).array().notNull(),
   gameCreation: bigint({ mode: "number" }).notNull(),
   gameStartTimestamp: bigint({ mode: "number" }).notNull(),
@@ -20,6 +21,7 @@ export const matches = pgTable("matches", {
   gameName: varchar({ length: 128 }).notNull(),
   gameType: varchar({ length: 32 }).notNull(),
   gameVersion: varchar({ length: 32 }).notNull(),
+  bluewardVersion: varchar({ length: 16 }).notNull(),
 })
 
 export const playerPerformances = pgTable("player_performances", {
@@ -31,48 +33,71 @@ export const playerPerformances = pgTable("player_performances", {
   puuid: varchar({ length: 128 }).notNull(),
   riotIdGameName: varchar({ length: 32 }).notNull(),
   riotIdTagline: varchar({ length: 8 }).notNull(),
+  payout: integer().notNull().default(0),
+  mmr: integer().notNull().default(0),
   champLevel: integer().notNull(),
   championId: integer().notNull(),
   championName: varchar({ length: 32 }).notNull(),
   profileIcon: integer().notNull(),
-  kills: integer().notNull(),
-  deaths: integer().notNull(),
-  assists: integer().notNull(),
-  killParticipation: doublePrecision().notNull(),
-  assistMePings: integer().notNull(),
-  totalPings: integer().notNull(),
-  doubleKills: integer().notNull(),
-  tripleKills: integer().notNull(),
-  quadraKills: integer().notNull(),
-  pentaKills: integer().notNull(),
-  killingSprees: integer().notNull(),
-  soloKills: integer().notNull(),
-  totalMinionsKilled: integer().notNull(),
-  teamDamagePercentage: doublePrecision().notNull(),
-  buffsStolen: integer().notNull(),
-  controlWardsPlaced: integer().notNull(),
-  damageTakenOnTeamPercentage: doublePrecision().notNull(),
-  earliestDragonTakedown: doublePrecision().notNull(),
-  epicMonsterSteals: integer().notNull(),
+  kills: smallint().notNull(),
+  deaths: smallint().notNull(),
+  assists: smallint().notNull(),
+  killParticipation: real().notNull(),
+
+  assistMePings: smallint().notNull(),
+  enemyMissingPings: smallint().notNull(),
+  enemyVisionPings: smallint().notNull(),
+  needVisionPings: smallint().notNull(),
+  onMyWayPings: smallint().notNull(),
+  pushPings: smallint().notNull(),
+
+  doubleKills: smallint().notNull(),
+  tripleKills: smallint().notNull(),
+  quadraKills: smallint().notNull(),
+  pentaKills: smallint().notNull(),
+  killingSprees: smallint().notNull(),
+  soloKills: smallint().notNull(),
+  totalMinionsKilled: smallint().notNull(),
+  teamDamagePercentage: real().notNull(),
+  buffsStolen: smallint().notNull(),
+
+  wardsPlaced: smallint().notNull(),
+  controlWardsPlaced: smallint().notNull(),
+  wardTakedowns: smallint().notNull(),
+  visionScore: smallint().notNull(),
+
+  spell1Casts: smallint().notNull(),
+  spell2Casts: smallint().notNull(),
+  spell3Casts: smallint().notNull(),
+  spell4Casts: smallint().notNull(),
+  summoner1Casts: smallint().notNull(),
+  summoner2Casts: smallint().notNull(),
+
+  totalHeal: integer().notNull(),
+  totalHealsOnTeammates: integer().notNull(),
+  totalDamageShieldedOnTeammates: integer().notNull(),
+  effectiveHealAndShielding: integer().notNull(),
+
+  damageTakenOnTeamPercentage: real().notNull(),
+  earliestDragonTakedown: integer().notNull(),
+  epicMonsterSteals: smallint().notNull(),
   firstTurretKilled: boolean().notNull(),
-  jungleCsBefore10Minutes: doublePrecision().notNull(),
-  killsNearEnemyTurret: integer().notNull(),
-  laneMinionsFirst10Minutes: integer().notNull(),
-  scuttleCrabKills: integer().notNull(),
-  survivedSingleDigitHpCount: integer().notNull(),
-  turretPlatesTaken: integer().notNull(),
-  turretTakedowns: integer().notNull(),
-  voidMonsterKill: integer().notNull(),
-  wardTakedowns: integer().notNull(),
+  jungleCsBefore10Minutes: smallint().notNull(),
+  killsNearEnemyTurret: smallint().notNull(),
+  laneMinionsFirst10Minutes: smallint().notNull(),
+  scuttleCrabKills: smallint().notNull(),
+  survivedSingleDigitHpCount: smallint().notNull(),
+  turretPlatesTaken: smallint().notNull(),
+  turretTakedowns: smallint().notNull(),
+  voidMonsterKill: smallint().notNull(),
   damageDealtToObjectives: integer().notNull(),
   damageDealtToTurrets: integer().notNull(),
   damageSelfMitigated: integer().notNull(),
-  enemyMissingPings: integer().notNull(),
   firstBloodKill: boolean().notNull(),
   firstTowerAssist: boolean().notNull(),
   firstTowerKill: boolean().notNull(),
   goldEarned: integer().notNull(),
-  inhibitorTakedowns: integer().notNull(),
+  inhibitorTakedowns: smallint().notNull(),
   item0: integer().notNull(),
   item1: integer().notNull(),
   item2: integer().notNull(),
@@ -81,33 +106,27 @@ export const playerPerformances = pgTable("player_performances", {
   item5: integer().notNull(),
   item6: integer().notNull(),
   largestCriticalStrike: integer().notNull(),
-  largestKillingSpree: integer().notNull(),
+  largestKillingSpree: smallint().notNull(),
   longestTimeSpentLiving: integer().notNull(),
   magicDamageDealtToChampions: integer().notNull(),
   magicDamageTaken: integer().notNull(),
-  neutralMinionsKilled: integer().notNull(),
-  onMyWayPings: integer().notNull(),
+  neutralMinionsKilled: smallint().notNull(),
   physicalDamageDealtToChampions: integer().notNull(),
   physicalDamageTaken: integer().notNull(),
-  summoner1Id: integer().notNull(),
-  summoner2Id: integer().notNull(),
-  summonerId: varchar({ length: 64 }).notNull(),
-  summonerLevel: integer().notNull(),
-  teamId: integer().notNull(),
-  totalDamageShieldedOnTeammates: integer().notNull(),
+  summoner1Id: smallint().notNull(),
+  summoner2Id: smallint().notNull(),
+  summonerLevel: smallint().notNull(),
+  teamId: smallint().notNull(),
   totalDamageTaken: integer().notNull(),
-  totalHeal: integer().notNull(),
-  totalHealsOnTeammates: integer().notNull(),
+
   totalTimeCCDealt: integer().notNull(),
   totalTimeSpentDead: integer().notNull(),
   trueDamageDealtToChampions: integer().notNull(),
   trueDamageTaken: integer().notNull(),
-  turretKills: integer().notNull(),
-  visionScore: integer().notNull(),
-  wardsPlaced: integer().notNull(),
+  turretKills: smallint().notNull(),
   win: boolean().notNull(),
-  keystonePerkId: integer().notNull(), // perks.styles[0].selections[0].perk
-  secondaryStyleId: integer().notNull(), // perks.styles[1].style
+  keystonePerkId: smallint().notNull(), // perks.styles[0].selections[0].perk
+  secondaryStyleId: smallint().notNull(), // perks.styles[1].style
 })
 
 export const teamObjectives = pgTable("team_objectives", {
@@ -116,76 +135,61 @@ export const teamObjectives = pgTable("team_objectives", {
   matchRowId: integer()
     .notNull()
     .references(() => matches.id, { onDelete: "cascade" }),
-  teamId: integer().notNull(),
+  teamId: smallint().notNull(),
   win: boolean().notNull(),
-  feat_epic_monster_kill_state: integer().notNull(),
-  feat_first_blood_state: integer().notNull(),
-  feat_first_turret_state: integer().notNull(),
-  atakhan_first: boolean().notNull(),
-  atakhan_kills: integer().notNull(),
-  baron_first: boolean().notNull(),
-  baron_kills: integer().notNull(),
-  champion_first: boolean().notNull(),
-  champion_kills: integer().notNull(),
-  dragon_first: boolean().notNull(),
-  dragon_kills: integer().notNull(),
-  horde_first: boolean().notNull(),
-  horde_kills: integer().notNull(),
-  inhibitor_first: boolean().notNull(),
-  inhibitor_kills: integer().notNull(),
-  riftHerald_first: boolean().notNull(),
-  riftHerald_kills: integer().notNull(),
-  tower_first: boolean().notNull(),
-  tower_kills: integer().notNull(),
+  featEpicMonsterKillState: smallint().notNull(),
+  featFirstBloodState: smallint().notNull(),
+  featFirstTurretState: smallint().notNull(),
+  atakhanFirst: boolean().notNull(),
+  atakhanKills: smallint().notNull(),
+  baronFirst: boolean().notNull(),
+  baronKills: smallint().notNull(),
+  championFirst: boolean().notNull(),
+  championKills: smallint().notNull(),
+  dragonFirst: boolean().notNull(),
+  dragonKills: smallint().notNull(),
+  hordeFirst: boolean().notNull(),
+  hordeKills: smallint().notNull(),
+  inhibitorFirst: boolean().notNull(),
+  inhibitorKills: smallint().notNull(),
+  riftHeraldFirst: boolean().notNull(),
+  riftHeraldKills: smallint().notNull(),
+  towerFirst: boolean().notNull(),
+  towerKills: smallint().notNull(),
 })
+
+export const roleEnum = pgEnum("role", ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "SUPPORT", "FILL"])
+
+export const rankTierEnum = pgEnum("rank_tier", [
+  "UNRANKED",
+  "IRON",
+  "BRONZE",
+  "SILVER",
+  "GOLD",
+  "PLATINUM",
+  "EMERALD",
+  "DIAMOND",
+  "MASTER",
+  "GRANDMASTER",
+  "CHALLENGER",
+])
 
 export const players = pgTable("players", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  authId: varchar({ length: 128 }).unique(),
+  bannerId: integer().notNull().default(0),
   createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
-  lastPlayedAt: timestamp({ withTimezone: true }).notNull(),
-  pointHistory: integer().array().notNull(),
+  aliases: varchar({ length: 64 })
+    .notNull()
+    .array()
+    .default(sql`ARRAY[]::varchar[]`),
+  primaryRole: roleEnum("primary_role").notNull().default("FILL"),
+  secondaryRole: roleEnum("secondary_role").notNull().default("FILL"),
+  rankTier: rankTierEnum("rank_tier").notNull().default("UNRANKED"),
+  lastPlayedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
   puuid: varchar({ length: 128 }).notNull().unique(),
   riotIdGameName: varchar({ length: 32 }).notNull(),
   riotIdTagline: varchar({ length: 8 }).notNull(),
-  matchHistory: varchar({ length: 32 }).array().notNull(),
-  recentChampionNames: varchar({ length: 32 }).array().notNull(),
-  gamesWon: integer().notNull(),
-  gamesLost: integer().notNull(),
-})
-
-export const tournamentStatus = pgEnum("tournament_status", ["upcoming", "running", "completed"])
-
-export const tournaments = pgTable("tournaments", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
-  name: varchar({ length: 128 }).notNull(),
-  status: tournamentStatus().notNull().default("upcoming"),
-})
-
-export const tournamentTeams = pgTable("tournament_teams", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
-  tournamentId: integer()
-    .notNull()
-    .references(() => tournaments.id, { onDelete: "cascade" }),
-  name: varchar({ length: 64 }).notNull(),
-})
-
-export const tournamentTeamPlayers = pgTable("tournament_team_players", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
-
-  tournamentId: integer()
-    .notNull()
-    .references(() => tournaments.id, { onDelete: "cascade" }),
-
-  teamId: integer()
-    .notNull()
-    .references(() => tournamentTeams.id, { onDelete: "cascade" }),
-
-  playerId: integer()
-    .notNull()
-    .references(() => players.id, { onDelete: "cascade" }),
 })
 
 export const matchesRelations = relations(matches, ({ many }) => ({
@@ -204,33 +208,5 @@ export const playerPerformancesRelations = relations(playerPerformances, ({ one 
   match: one(matches, {
     fields: [playerPerformances.matchRowId],
     references: [matches.id],
-  }),
-}))
-
-export const tournamentsRelations = relations(tournaments, ({ many }) => ({
-  teams: many(tournamentTeams),
-  roster: many(tournamentTeamPlayers),
-}))
-
-export const tournamentTeamsRelations = relations(tournamentTeams, ({ one, many }) => ({
-  tournament: one(tournaments, {
-    fields: [tournamentTeams.tournamentId],
-    references: [tournaments.id],
-  }),
-  members: many(tournamentTeamPlayers),
-}))
-
-export const tournamentTeamPlayersRelations = relations(tournamentTeamPlayers, ({ one }) => ({
-  tournament: one(tournaments, {
-    fields: [tournamentTeamPlayers.tournamentId],
-    references: [tournaments.id],
-  }),
-  team: one(tournamentTeams, {
-    fields: [tournamentTeamPlayers.teamId],
-    references: [tournamentTeams.id],
-  }),
-  player: one(players, {
-    fields: [tournamentTeamPlayers.playerId],
-    references: [players.id],
   }),
 }))
