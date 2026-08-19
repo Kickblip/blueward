e71c2f205649b96a4f7f1cdbbb970a7e13b15da4
Kickblip <wyatt.zilker@gmail.com>
2026-08-19 09:31:11 -0500

    More simple members settings page

--- app/(with-sidebar)/clubs/[slug]/page.tsx
+++ app/(with-sidebar)/clubs/[slug]/page.tsx
@@ -59,61 +59,11 @@ export default async function Page({
             bannerId={member.playerCard ? member.playerCard.bannerId : 0}
           >
             <div className="min-h-0 flex-1 overflow-hidden rounded-md border">
-              <div className="flex h-full flex-col justify-between gap-2 p-2">
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-red-500">HELLO</div>
-
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button variant="secondary" size="icon-sm">
-                        <EllipsisIcon />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-40" align="end">
-                      <DropdownMenuGroup>
-                        <DropdownMenuLabel>Player</DropdownMenuLabel>
-                        <DropdownMenuItem>
-                          <UserIcon />
-                          View Profile
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                          <HandIcon />
-                          Draft Player
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuGroup>
-                        <DropdownMenuLabel>Admin</DropdownMenuLabel>
-                        <DropdownMenuItem>
-                          <ArrowLeft />
-                          Move to Team 1
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                          <ArrowRight />
-                          Move to Team 2
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuGroup>
-                        <DropdownMenuLabel>Danger</DropdownMenuLabel>
-                        <DropdownMenuItem variant="destructive">
-                          <CrownIcon />
-                          Make Owner
-                        </DropdownMenuItem>
-                        <DropdownMenuItem variant="destructive">
-                          <Trash2Icon />
-                          Kick Player
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
-                <h2 className="font-oswald text-4xl font-semibold text-white uppercase">
-                  {member.playerCard
-                    ? member.playerCard.riotIdGameName
-                    : "Unknown"}
-                </h2>
-              </div>
+              <h2 className="font-oswald text-4xl font-semibold text-white uppercase">
+                {member.playerCard
+                  ? member.playerCard.riotIdGameName
+                  : "Unknown"}
+              </h2>
             </div>
           </BannerBackground>
         ))}
